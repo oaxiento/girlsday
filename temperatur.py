603d00765d83071e3e3099c73a75d53fde051ec4
MM <15368510+oaxiento@users.noreply.github.com>
2025-04-03 09:34:35 +0200

    Remove code, add comments

--- temperatur.py
+++ temperatur.py
@@ -15,8 +15,7 @@ import matplotlib.animation as animation
 ########################################
 
 # Definiere wo das Programm die Sensordaten im Dateisystem findet
-temperatur_sensor_ordner = glob.glob('/sys/bus/w1/devices/' + '28*')[0]
-temperatur_sensor_datei = temperatur_sensor_ordner + '/temperature'
+### HIER FEHLT NOCH WAS ###
 
 def diagramm(i, xs, ys):
 
@@ -25,54 +24,43 @@ def diagramm(i, xs, ys):
     ##########################################
 
     # Lese die Datei, die die Sensordaten enthält
-    sensor_datei = open(temperatur_sensor_datei, 'r')
-    sensor_daten = sensor_datei.readlines()
-    sensor_datei.close()
+    ### HIER FEHLT NOCH WAS ###
 
     # Lese den aktuellen Sensorwert aus den Sensordaten
-    sensorwert = sensor_daten[0]
+    ### HIER FEHLT NOCH WAS ###
 
     # Wandle den Sensorwert in Grad Celsius um
-    temperatur_celsius = float(sensorwert) / 1000.0
+    ### HIER FEHLT NOCH WAS ###
 
     ########################
     ## Diagramm erstellen ##
     ########################
 
     # Füge die aktuelle Zeit zur x-Achse und den Temperaturwert in Grad Celsius zur y-Achse des Diagramms hinzu
-    xs.append(dt.datetime.now().strftime('%H:%M:%S'))
-    ys.append(temperatur_celsius)
+    ### HIER FEHLT NOCH WAS ###
 
     # Kürze die Werte des Diagrams auf maximal 10 Elemente
-    xs = xs[-10:]
-    ys = ys[-10:]
+    ### HIER FEHLT NOCH WAS ###
 
     # Schreibe die Werte auf die Achsen
-    ax.clear()
-    ax.plot(xs, ys)
+    ### HIER FEHLT NOCH WAS ###
 
     # Füge Titel und Achsenbeschriftungen zum Diagramm hinzu
-    plt.title('Temperaturdiagramm')
-    plt.ylabel('Temperatur (C°)')
-    plt.xticks(rotation=90, ha='center', fontsize=10)
-    plt.subplots_adjust(bottom=0.20)
+    ### HIER FEHLT NOCH WAS ###
 
 ##########################
 ## Diagramm formatieren ##
 ##########################
 
 # Lege fest wie das Diagramm aussehen soll und wie groß das Programmfenster sein soll
-fig = plt.figure(facecolor='white', figsize=(10, 5))
-ax = fig.add_subplot(1, 1, 1)
-xs = []
-ys = []
+### HIER FEHLT NOCH WAS ###
 
 ###########################################
 ## Programmfenster mit Diagramm anzeigen ##
 ###########################################
 
 # Erstelle eine Animation, die das formatierte Diagramm und die Temperaturdaten jede Sekunde (alle 1000 Millisekunden) aktualisiert
-ani = animation.FuncAnimation(fig, diagramm, fargs=(xs, ys), interval=1000)
+### HIER FEHLT NOCH WAS ###
 
 # Öffne ein Fenster mit dem Diagramm und zeige es an bis es geschlossen wird
-plt.show()
+### HIER FEHLT NOCH WAS ###
